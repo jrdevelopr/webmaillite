2ea965185b5188d0712ed5161e3e265d60fffad2
JrDevelopr <jrdevelopr@gmail.com>
2021-11-29 22:16:16 -0500

    add composer files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a561d3439f702c60da66cfad9d08c960d1787432',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a561d3439f702c60da66cfad9d08c960d1787432',
+            'dev_requirement' => false,
+        ),
+    ),
+);
